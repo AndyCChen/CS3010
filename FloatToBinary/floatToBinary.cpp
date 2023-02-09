d6c80a076ca386b72306f559c05aade09df6a94d
Andy Chen <acchen@cpp.edu>
2023-02-09 00:12:31 -0800

    save work

--- FloatToBinary/floatToBinary.cpp
+++ FloatToBinary/floatToBinary.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
-int main() {
+using namespace std;
 
-   return 0;
+int main() {
+	int choice;
+
+	do {
+		cout << "Select one of the following formating for input." << endl;
+		cout << "1) a.b * 10^(e)      Ex: 3.14e2" << endl;
+		cout << "2) a.b               Ex: 10.25" << endl;
+		cout << "Choice: ";
+		cin >> choice;
+	} while (choice != 1 && choice != 2);
+
+	
+
+	return 0;
 }
