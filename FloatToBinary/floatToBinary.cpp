8dcac6844d57dad554947636e697c6295a8b5c0b
Andy Chen <acchen@cpp.edu>
2023-02-10 08:43:18 -0800

    save work

--- FloatToBinary/floatToBinary.cpp
+++ FloatToBinary/floatToBinary.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-	int choice;
-
-	do {
-		cout << "Select one of the following formating for input." << endl;
-		cout << "1) a.b * 10^(e)      Ex: 3.14e2" << endl;
-		cout << "2) a.b               Ex: 10.25" << endl;
-		cout << "Choice: ";
-		cin >> choice;
-	} while (choice != 1 && choice != 2);
-
+void validateInput(string input) {
+	string exponent_delimiter = "e";
+	size_t exponent_position = input.find(exponent_delimiter);
 	
+	if (exponent_position == string::npos) return;
+
+	string decimal_string = input.substr(0, exponent_position);
+	string power_string = input.substr(exponent_position + 1, input.length() - 1);
+
+	double decimal = stod(decimal_string);
+	int power = stoi(power_string);
+
+	cout << "decimal: " << decimal << endl;
+	cout << "power: " << power << endl;
+	
+}
+
+int main() {
+	string input;
+
+	cout << "Enter a number in one of the following formats." << endl;
+	cout << "1) a.b * 10^(e)      Ex: 3.14e2" << endl;
+	cout << "2) a.b               Ex: 10.25" << endl;
+	cout << "Input: ";
+	cin >> input;
+
+	validateInput(input);
 
 	return 0;
 }
