89c8f3918376c0b5bd121e358c45faf751e816b2
AndyCChen <acchen@cpp.edu>
2023-02-23 23:42:25 -0800

    save work

--- Gaussian/gaussian.cpp
+++ Gaussian/gaussian.cpp
@@ -2,9 +2,29 @@
 
 using namespace std;
 
+char handle_program_arguments(int argc, char *argv[]) {
+   if (argc == 1) {
+      return '0';
+   }
+
+   if (argc == 2) {
+      if (strcmp(argv[1], "--spp") != 0) {
+         return '0';
+      }
+   }
+
+   return '1';
+}
+
 int main(int argc, char *argv[]) {
-   cout << argc << endl;
-   cout << "Flag: " << argv[1] << endl;
+   char choice = handle_program_arguments(argc, argv);
+
+   switch(choice) {
+      case '0':
+         cout << "Error, missing input file." << endl;
+         return 1;
+      
+   }
 
    return 0;
 }
