ffc6ca108cc75541dc07ded8381e61b05ba4307b
Andy Chen <acchen@cpp.edu>
2023-02-24 22:46:50 -0800

    finish

--- Gaussian/gaussian.cpp
+++ Gaussian/gaussian.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -9,6 +11,7 @@ int handle_program_arguments(int, char*[]);
 double** read_input_file(string, int&);
 double* gaussian_elimination(double**, int);
 double* guassian_elimination_with_spp(double**, int);
+void output_solution(double*, int);
 
 int main(int argc, char *argv[]) {
    int choice = handle_program_arguments(argc, argv);
@@ -27,21 +30,32 @@ int main(int argc, char *argv[]) {
          
          if (matrix != nullptr) {
             solutions = gaussian_elimination(matrix, size_n);
-            for (int row = 0; row < size_n + 1; row++) {
-               for (int col = 0; col < size_n; col++) {
-                  cout << matrix[row][col] << " ";
-               }
-               cout << endl;
-            }
-
-            for (int row = 0; row < size_n; row++) {
-               cout << solutions[row] << " ";
-            }
+            output_solution(solutions, size_n);
          }
-         
+
+         delete[] solutions;
+         for (int i = 0; i < size_n; i++) {
+            delete[] matrix[i];
+         }
+         delete[] matrix; 
+   
          break;
       case 2:
          cout << "Running gaussian elimination with partial pivoting." << endl;
+
+         matrix = read_input_file(argv[2], size_n);
+
+         if (matrix != nullptr) {
+            solutions = guassian_elimination_with_spp(matrix, size_n);
+            output_solution(solutions, size_n);
+         }
+
+         delete[] solutions;
+         for (int i = 0; i < size_n; i++) {
+            delete[] matrix[i];
+         }
+         delete[] matrix; 
+
          break;
       default:
          cout << "Error, invalid argument(s)." << endl;
@@ -127,6 +141,8 @@ double** read_input_file(string file_name, int &size_n) {
          col = 0;
          row++;
       }
+
+      file.close();
    } else {
       cout << "Failed to open file." << endl;
       return nullptr;
@@ -136,6 +152,7 @@ double** read_input_file(string file_name, int &size_n) {
 }
 
 double* gaussian_elimination(double **matrix, int size_n) {
+   // fwd elimination
    for (int col_coefficient_index = 0; col_coefficient_index < size_n - 1; col_coefficient_index++) {
       for (int row_coefficient_index = col_coefficient_index + 1; row_coefficient_index < size_n; row_coefficient_index++) {
 
@@ -155,6 +172,7 @@ double* gaussian_elimination(double **matrix, int size_n) {
    double *solutions = new double[size_n];
    solutions[size_n - 1] = matrix[size_n][size_n - 1] / matrix[size_n - 1][size_n - 1];
 
+   // backwards substitution
    for (int row = size_n - 2; row >= 0; row--) {
       double sum = matrix[size_n][row];
 
@@ -169,5 +187,79 @@ double* gaussian_elimination(double **matrix, int size_n) {
 }
 
 double* guassian_elimination_with_spp(double **matrix, int size_n) {
-   
+   int *indices = new int[size_n];
+   double *scaling = new double[size_n];
+
+   // find greatest abs coefficient from each row
+   for (int row = 0; row < size_n; row++) {
+      double smax = 0;
+      indices[row] = row;
+
+      for (int col = 0; col < size_n; col++) {
+         smax = max(smax, abs(matrix[row][col]));
+      }
+
+      scaling[row] = smax;
+   }
+
+   // fwd elimination
+   for (int k = 0; k < size_n - 1; k++) {
+      double max_ratio = 0;
+      int max_index = k;
+
+      for (int i = k; i < size_n; i++) {
+         double ratio = matrix[indices[i]][k] / scaling[indices[i]];
+
+         if (ratio > max_ratio) {
+            max_ratio = ratio;
+            max_index = i;
+         }
+      }
+
+      // swap
+      int temp = indices[max_index];
+      indices[max_index] = indices[k];
+      indices[k] = temp;
+
+       for (int i = k + 1; i < size_n; i++) {
+         double mult = matrix[indices[i]][k] / matrix[indices[k]][k];
+         
+         for (int j = k + 1; j < size_n; j++) {
+            matrix[indices[i]][j] = matrix[indices[i]][j] - mult * matrix[indices[k]][j];
+         }
+
+         matrix[size_n][indices[i]] = matrix[size_n][indices[i]] - mult * matrix[size_n][indices[k]];
+      } 
+   }
+
+   double *solutions = new double[size_n];
+
+   // backwards substitution
+   solutions[0] = matrix[size_n][indices[size_n]];
+
+   for (int i = size_n - 2; i >= 0; i--) {
+      double sum = matrix[size_n][indices[i]];
+      
+      for (int j = i + 1; j < size_n; j++) {
+         sum = sum - matrix[indices[i]][j] * solutions[j];
+      }
+      
+      solutions[i] = sum / matrix[indices[i]][i];
+   }
+
+   delete[] indices;
+   delete[] scaling;
+
+   return solutions;
+}
+
+void output_solution(double *solution, int size_n) {
+   ofstream solution_file;
+   solution_file.open("sys.sol");
+
+   for (int i = 0; i < size_n; i++) {
+      solution_file << solution[i] << " ";
+   }
+
+   solution_file.close();
 }
